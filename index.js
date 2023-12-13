895606a0f83b13542c9ac4a4de996bf4ba13f009
Miki Stec <mikulas@stec.sk>
2023-12-13 20:38:14 +0100

    basic minimal server

--- index.js
+++ index.js
@@ -1,39 +1,11 @@
 import express from 'express';
-import cors from 'cors';
-import contactsRouter from './api/contacts.route.js';
-import mongodb from "mongodb"
-import dotenv from "dotenv"
-import ContactsDAO from './api/contactsDAO.js'
+const app = express();
+const port = 3000;
 
-async function main() {
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
-  dotenv.config()
-
-  const client = new mongodb.MongoClient(process.env.DB_URI)
-  const port = process.env.PORT
-  const url = process.env.URL
-
-  try {
-    await client.connect()
-    await ContactsDAO.injectDB(client)
-
-    const app = express();
-    app.use(cors());
-    app.use(express.json());
-    app.use(url, contactsRouter);
-    app.use('*', (req, res) => {
-      res.status(404).json({ error: "not found" })
-    })
-
-    app.listen(port, () => {
-      console.log('server is running on port:' + port);
-    })
-
-  }
-  catch (e) {
-    console.error(e);
-    process.exit(1)
-  }
-}
-
-main().catch(console.error);
+app.listen(3000, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
